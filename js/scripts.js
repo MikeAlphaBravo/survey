16dd070342c77d53a2269108ed37c67fed5960ce
Michael Brooks <mikeyabrooks@gmail.com>
2017-06-08 19:47:50 -0700

    Structure all calculator like function in both HTML and JS, it will now ad and return a result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+var add = function(number1, number2, number3, number4, number5) {
+  return number1 + number2 + number3 + number4 + number5;
+};
+
+
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var clientNameInput = $("input#clientName").val();
@@ -8,11 +13,24 @@ $(document).ready(function() {
     $(".clientEmail").text(clientEmailInput);
     $(".appointmentDate").text(appointmentDateInput);
 
-    var flavor = $("input:radio[name=flavor]:checked").val();
-    var flavor = $("input:radio[name=flavor]:checked").val();
-    var flavor = $("input:radio[name=flavor]:checked").val();
-    var flavor = $("input:radio[name=flavor]:checked").val();
-    var flavor = $("input:radio[name=flavor]:checked").val();
+    var spice = $("input:radio[name=spice]:checked").val();
+    var vacation = $("input:radio[name=vacation]:checked").val();
+    var adventure = $("input:radio[name=adventure]:checked").val();
+    var excites = $("input:radio[name=excites]:checked").val();
+    var length = $("input:radio[name=length]:checked").val();
+
+
+    $("form#add").submit(function(event) {
+      var number1 = parseInt($("#spice").val());
+      var number2 = parseInt($("#vacation").val());
+      var number3 = parseInt($("#adventure").val());
+      var number4 = parseInt($("#excites").val());
+      var number5 = parseInt($("#length").val());
+      var result = (add(number1, number2, number3, number4, number5));
+      $("#output").text(result);
+      event.preventDefault();
+    });
+
 
     $("#confirmation").show();
     $("#emptyForm").hide();
